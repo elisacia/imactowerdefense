3b3c374b9bc07a4708d0de63569790589203167f
Elisa <ciavaldini.e@gmail.com>
2019-04-30 16:35:29 +0200

    TOWER.PNG
    - images niches
    - description niches

---
+++ include/jeu.h
@@ -0,0 +1,15 @@
+#ifndef ITD_JEU__
+#define ITD_JEU__
+
+/* ------- STRUCTURES ------- */
+
+typedef struct jeu {
+	int start;
+	int gagne;
+	int perdu;
+	int pause;
+	int help;
+	int regle;
+}Jeu;
+
+#endif

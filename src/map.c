c7729bbb735e8cb2247c42b1a0065754e07bd27a
Elisa <ciavaldini.e@gmail.com>
2019-05-09 16:37:41 +0200

    DEBUG VERIFICATION

--- src/map.c
+++ src/map.c
@@ -15,46 +15,67 @@ int createMap(FILE* fichierITD, Map* map){
 		return 0;
 	}
 
-		//vérification commentaire
-
-	//vérification format ppm
-	char filename[20];
-	fgets(filename,20, fichierITD);
-	char* extension = strchr(filename,'.');
-	if (strcmp(extension,".ppm\n") !=0)
-	{
-		fprintf(stderr, "Erreur ITD: Le fichier n'est pas au format .ppm \n" );
-		return 0;
-	}
-	else {
-		(*map).map = filename;
-	}
-
-	//vérification version
+		//vérification version
 	int version;
-	fscanf(fichierITD,"%d\n",version);
+
+	fscanf(fichierITD,"%d\n",&version);
+
 	if (version !=1)
 	{
 		fprintf(stderr,  "Erreur ITD : version\n");
 		return 0;
 	}
 
+		//vérification commentaire
+
+	char diese[0];
+	fscanf(fichierITD,"%c",&diese);
+	if (strcmp(diese,"#") !=0)
+	{
+		fprintf(stderr,  "Erreur ITD : Ligne de commentaire\n");
+		return 0;
+	}
+
+		char commentaire[100];
+		fgets(commentaire,100,fichierITD);
+
 	//vérification de mots clés: initialisation
-	char keyword[10];
+	char keyword[4];
 	int R = 0;
 	int G = 0;
 	int B = 0;
 
 	//vérification mot clé : "carte"
 	fgets(keyword,7, fichierITD);
-	if (strcmp(keyword,"carte") !=0)
+
+	if (strcmp(keyword,"carte ") !=0)
 	{
 		fprintf(stderr, "Erreur ITD: Mot-clé différent de \"carte\" \n" );
 		return 0;
 	}
 
+	//vérification format ppm
+	char filename[20];
+
+	fgets(filename,20, fichierITD);
+
+fprintf(stderr, "%s\n",filename );
+	char* extension = strchr(filename,'.');
+
+
+	if (strcmp(extension,".ppm\n") !=0)
+	{
+
+		fprintf(stderr, "Erreur ITD: Le fichier n'est pas au format .ppm \n" );
+		return 0;
+	}
+	else {
+
+		(*map).map = filename;
+	}
+
 	//vérification mot clé : "chemin"
-	fgets(keyword, 8, fichierITD);
+	fgets(keyword, 7, fichierITD);
 	if (strcmp(keyword,"chemin")== 0)
 	{
 		fscanf(fichierITD, "%d %d %d\n", &R, &G, &B);
@@ -74,7 +95,7 @@ int createMap(FILE* fichierITD, Map* map){
 
 
 	//vérification mot clé : "noeud"
-	fgets(keyword, 7, fichierITD);
+	fgets(keyword, 6, fichierITD);
 	if (strcmp(keyword,"noeud")==0) 
 	{
 		fscanf(fichierITD, "%d %d %d\n", &R, &G, &B);
@@ -94,7 +115,7 @@ int createMap(FILE* fichierITD, Map* map){
 
 
 	//vérification mot clé : "construct"
-	fgets(keyword, 11, fichierITD);
+	fgets(keyword, 10, fichierITD);
 	if (strcmp(keyword,"construct")==0) 
 	{
 		fscanf(fichierITD, "%d %d %d\n", &R, &G, &B);
@@ -114,7 +135,7 @@ int createMap(FILE* fichierITD, Map* map){
 
 
 	//vérification mot clé : "in"
-	fgets(keyword, 4, fichierITD);
+	fgets(keyword, 3, fichierITD);
 	if (strcmp(keyword,"in")==0) 
 	{
 		fscanf(fichierITD, "%d %d %d\n", &R, &G, &B);
@@ -134,7 +155,7 @@ int createMap(FILE* fichierITD, Map* map){
 
 
 	//vérification mot clé : "out"
-	fgets(keyword, 5, fichierITD);
+	fgets(keyword, 4, fichierITD);
 	if (strcmp(keyword,"out")==0) 
 	{
 		fscanf(fichierITD, "%d %d %d\n", &R, &G, &B);
@@ -157,21 +178,16 @@ int createMap(FILE* fichierITD, Map* map){
 	int nbNoeud;
 	fscanf(fichierITD, "%d\n", &nbNoeud);
 
-
-	//récupération position du curseur
-	long position;
-	position = ftell(fichierITD);
-
-
 	//récupération du nombre de coordonnées de noeuds (on compte le nombre de ligne qu'il reste)
-	int nbLines = 0;
-	while((fgetc(fichierITD)) != EOF) {
-		if (fgetc(fichierITD) == '\n') 
+	unsigned int nbLines = 1;
+	int c;
+	while((c = fgetc(fichierITD)) != EOF) {
+		if (c == '\n') 
 		{
 			nbLines++;
 		}
 	}
-	nbLines++;
+
 	if (nbNoeud != nbLines)
 	{
 		fprintf(stderr, "Erreur ITD : Nombre de noeud\n");
@@ -181,6 +197,11 @@ int createMap(FILE* fichierITD, Map* map){
 		(*map).nbNodes = nbNoeud;
 	}
 
+	//récupération position du curseur
+	long position;
+	position = ftell(fichierITD);
+
+
 	fseek(fichierITD, position, SEEK_SET); //place le cuseur au premier noeud
 
 	//suppression du '\n' dans le nom de l'image
@@ -188,7 +209,7 @@ int createMap(FILE* fichierITD, Map* map){
 	filename[length-1] = '\0';
 
 	//chargement carte 
-	char file[30] = "images/";
+	char file[30] = "img/";
 	strcat(file,filename);
 	SDL_Surface* carte = IMG_Load(file);
 	if (carte == NULL)
@@ -236,6 +257,7 @@ int createMap(FILE* fichierITD, Map* map){
 
 Map loadMap(char* nameITD) {
 	FILE* fichierITD = fopen(nameITD,"r");
+
 	if (fichierITD == NULL)
 	{
 		fprintf(stderr, "Erreur dans l'ouverture du fichier ITD %s\n",nameITD );
@@ -243,7 +265,7 @@ Map loadMap(char* nameITD) {
 	}
 
 	Map map;
-
+		
 	if (createMap(fichierITD, &map))
 	{
 		return map;
@@ -252,6 +274,7 @@ Map loadMap(char* nameITD) {
 		fprintf(stderr, "Erreur le fichier ITD %s n'est pas valide\n",nameITD );
 		exit(1);
 	}
+
 }
 
 
